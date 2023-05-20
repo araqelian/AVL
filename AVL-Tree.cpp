981a15d24b4ee3a676333ab539976c950efdbc05
Davit Arakelyan <darakely@c2r5s1.42yerevan.am>
2023-05-20 14:31:48 +0400

    Insert Delete Search

---
+++ AVL-Tree.cpp
@@ -0,0 +1,264 @@
+#include "./AVL-Tree.hpp"
+
+/********************/
+/*		Public		*/
+/********************/
+
+AVL::AVL()
+{
+	this->root = nullptr;
+}
+
+AVL::~AVL()
+{
+	//
+}
+
+void	AVL::Insert(int data)
+{
+	Node*	tmp_root = this->root;
+	this->root = Insert_hepler(tmp_root, data);
+}
+
+void	AVL::Delete(int data)
+{
+	if (!this->root)
+	{
+		cout << "AVL-Tree is a empty." << endl;
+		return ;
+	}
+
+	Node*	tmp = Search(data);
+	if (tmp)
+		Delete_helper(tmp, data);
+}
+
+Node*	AVL::Search(int data)
+{
+	if (!this->root)
+	{
+		cout << "AVL-Tree is a empty." << endl;
+		return nullptr;
+	}
+
+	Node*	tmp_root = this->root;
+	return Search_helper(tmp_root, data);
+}
+
+void	AVL::Inorder_Traversal()
+{
+	if (!this->root)
+	{
+		cout << "AVL-Tree is a empty." << endl;
+		return;
+	}
+
+	Node*	tmp_root = this->root;
+	Inorder_Traversal_helper(tmp_root);
+}
+
+/********************/
+/*		Private		*/
+/********************/
+
+
+Node*	AVL::Insert_hepler(Node* node, int data)
+{
+	if (!node)
+		return new Node(data);
+	
+	if (data < node->data)
+		node->left = Insert_hepler(node->left, data);
+	else if (data > node->data)
+		node->right = Insert_hepler(node->right, data);
+	else
+	{
+		cout << "Dublicate number forbide." << endl;
+		return node;
+	}
+
+	node->height = 1 + Max(Height(node->left), Height(node->right));
+
+	int	BF = Height(node->left) - Height(node->right);
+
+	if (BF > 1)
+	{
+		if (data < node->left->data)
+			return Right_Rotate(node);
+		else if (data > node->left->data)
+		{
+			node->left = Left_Rotate(node->left);
+			return Right_Rotate(node);
+		}
+	}
+	else if (BF < -1)
+	{
+		if (data > node->right->data)
+			return Left_Rotate(node);
+		else if (data < node->right->data)
+		{
+			node->right = Right_Rotate(node->right);
+			return Left_Rotate(node);
+		}
+	}
+
+	return node;
+}
+
+int	AVL::Max(int a, int b)
+{
+	return a > b ? a : b;
+}
+
+int	AVL::Height(Node* node)
+{
+	if (!node)
+		return 0;
+	return node->height;
+}
+
+Node*	AVL::Delete_helper(Node* node, int data)
+{
+	if (!node)
+		return node;
+	if (data < node->data)
+		node->left = Delete_helper(node->left, data);
+	else if (data > node->data)
+		node->right = Delete_helper(node->right, data);
+	else
+	{
+		if (!node->left || !node->right)
+		{
+			Node*	tmp = node->left ? node->left : node->right;
+
+			if (!tmp)
+			{
+				tmp = node;
+				node = nullptr;
+			}
+			else
+				*node = *tmp;
+			delete tmp;
+		}
+		else
+		{
+			Node*	min = MinNode(node->right);
+
+			node->data = min->data;
+			node->right = Delete_helper(node->right, min->data);
+		}
+	}
+
+	if (!node)
+		return node;
+
+	node->height = 1 + Max(Height(node->left), Height(node->right));
+
+	int BF = BalanceFactor(node);
+
+	if (BF > 1)
+	{
+		if (BalanceFactor(node->left) >= 0)
+			return Right_Rotate(node);
+		else
+		{
+			node->left = Left_Rotate(node->left);
+			return Right_Rotate(node);
+		}
+	}
+	if (BF < -1)
+	{
+		if (BalanceFactor(node->right) <= 0)
+			return Left_Rotate(node);
+		else
+		{
+			node->right = Right_Rotate(node->right);
+			return Left_Rotate(node);
+		}
+	}
+
+	return node;
+}
+
+Node*	AVL::MinNode(Node* node)
+{
+	Node*	tmp = node;
+	while (tmp->left)
+		tmp = tmp->left;
+	return tmp;
+}
+
+Node*	AVL::MaxNode(Node* node)
+{
+	Node*	tmp = node;
+	while (tmp->right)
+		tmp = tmp->right;
+	return tmp;
+}
+
+int		AVL::BalanceFactor(Node* node)
+{
+	if (!node)
+		return 0;
+	return Height(node->left) - Height(node->right);
+}
+
+Node*	AVL::Search_helper(Node* node, int data)
+{
+	if (!node)
+	{
+		cout << data << " not found." << endl;
+		return nullptr;
+	}
+
+	if (node->data == data)
+		return node;
+
+	if (data < node->data)
+		return Search_helper(node->left, data);
+	return Search_helper(node->right, data);
+}
+
+Node*	AVL::Left_Rotate(Node* x)
+{
+	Node*	y = x->right;
+	Node*	T2 = y->left;
+
+	y->left = x;
+	x->right = T2;
+
+	x->height = 1 + Max(Height(x->left), Height(x->right));
+	y->height = 1 + Max(Height(y->left), Height(y->right));
+
+	return y;
+}
+
+Node*	AVL::Right_Rotate(Node* x)
+{
+	Node*	y = x->left;
+	Node*	T2 = y->right;
+
+	y->right = x;
+	x->left = T2;
+
+	x->height = 1 + Max(Height(x->left), Height(x->right));
+	y->height = 1 + Max(Height(y->left), Height(y->right));
+
+	return y;
+}
+
+void	AVL::Inorder_Traversal_helper(Node* node)
+{
+	if (!node)
+		return;
+
+	Inorder_Traversal_helper(node->left);
+
+	if (node == this->root)
+		cout << "__ ROOT __" << endl;
+	
+	cout << "data:\t" << node->data << endl;
+	cout << "height:\t" << node->height << endl << endl;
+
+	Inorder_Traversal_helper(node->right);
+}
---
+++ AVL-Tree.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+struct Node
+{
+	Node*	left;
+	Node*	right;
+	int		height;
+	int		data;
+
+	Node(int new_data) : left(nullptr), right(nullptr), height(1), data(new_data) {}
+};
+
+class AVL
+{
+public:
+	AVL();
+	~AVL();
+
+	void	Insert(int data);
+	void	Delete(int data);
+	Node*	Search(int data);
+
+	void	Inorder_Traversal();
+
+private:
+	Node*	root;
+
+	Node*	Insert_hepler(Node* node, int data);
+	int		Max(int a, int b);
+	int		Height(Node* node);
+
+	Node*	Delete_helper(Node* node, int data);
+
+	Node*	MinNode(Node* node);
+	Node*	MaxNode(Node* node);
+
+	int		BalanceFactor(Node* node);
+
+	Node*	Search_helper(Node* node, int data);
+
+	Node*	Left_Rotate(Node* x);
+	Node*	Right_Rotate(Node* x);
+
+	void	Inorder_Traversal_helper(Node* node);
+};
